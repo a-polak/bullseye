3589b91949bdda1bb622d9d226661ea93b1ded9c
Adam Polak <adam.polak@zoho.com>
2020-06-04 16:04:30 +0200

    Text fields with default values filled.

--- app/src/main/java/pl/tenk/bullseye/MainActivity.java
+++ app/src/main/java/pl/tenk/bullseye/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+import static pl.tenk.bullseye.R.integer.alpha_value;
+import static pl.tenk.bullseye.R.integer.charlie_value;
+import static pl.tenk.bullseye.R.integer.delta_value;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
     Button startButton, stopButton, resetButton, addAlphaButton, addCharlieButton, addDeltaButton;
     long milisecondsTime, startTime, bufTime, updateTime = 0L;
     Handler stopwatchHandler;
-    int Seconds, Minutes, Miliseconds, updatePoints = 0;
-    float Factor = (float) 0.000;
-    float timeInSeconds = (float) 0.000;
+    int Seconds = 0, Minutes = 0, Miliseconds = 0, updatePoints = 0;
+    float Factor = (float) 0.0;
+    float timeInSeconds = (float) 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,16 @@ public class MainActivity extends AppCompatActivity {
         addCharlieButton = (Button)findViewById(R.id.button_add_charlie);
         addDeltaButton = (Button)findViewById(R.id.button_add_delta);
 
-
-  //      addAlphaButton.setText(String.format(getString(R.id.button_add_alpha, )));
+        timer.setText(getResources().getString(R.string.stopwatch_counter_zero,
+                Minutes,
+                Seconds,
+                Miliseconds));
+        addAlphaButton.setText(getResources().getString(R.string.button_add_alpha, getResources().getInteger(alpha_value)));
+        addCharlieButton.setText(getResources().getString(R.string.button_add_charlie, getResources().getInteger(charlie_value)));
+        addDeltaButton.setText(getResources().getString(R.string.button_add_delta, getResources().getInteger(delta_value)));
+        comstockCalculation.setText(getResources().getString(R.string.text_factorCalculation,
+                updatePoints,
+                timeInSeconds));
 
         stopwatchHandler = new Handler();
 
@@ -46,9 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 stopwatchHandler.postDelayed(runnableStopwatch, 0);
                 resetButton.setEnabled(false);
                 startButton.setEnabled(false);
-                addAlphaButton.setEnabled(false);
-                addCharlieButton.setEnabled(false);
-                addDeltaButton.setEnabled(false);
             }
         });
 
@@ -62,8 +72,15 @@ public class MainActivity extends AppCompatActivity {
                 addAlphaButton.setEnabled(true);
                 addCharlieButton.setEnabled(true);
                 addDeltaButton.setEnabled(true);
+                comstockCalculation.setText(getString(R.string.text_factorCalculation, updatePoints, timeInSeconds));
+                if(timeInSeconds >0)
+                Factor = (float) updatePoints / timeInSeconds;
+                else Factor = 0;
+                comstockFactor.setText(String.format(Locale.getDefault(),"%.3f", Factor));
+
             }
         });
+
         resetButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -71,26 +88,55 @@ public class MainActivity extends AppCompatActivity {
                 startTime = 0L;
                 bufTime = 0L;
                 updateTime = 0L;
+                timeInSeconds = 0f;
+                updatePoints = 0;
                 Minutes = 0;
                 Seconds = 0;
                 Miliseconds = 0;
                 timer.setText(getResources().getString(R.string.stopwatch_counter_zero));
                 comstockFactor.setText(getResources().getString(R.string.factor_value_zero));
-                updatePoints = 0;
-            }
-        });
-        addAlphaButton.setText(getString(R.string.button_alpha));
-        addAlphaButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                updatePoints += getResources().getInteger(R.integer.alpha_value);
-                comstockCalculation.setText(String.format("%d", updatePoints) + " / " + String.format("%.3f", timeInSeconds));
-                Factor = updatePoints / timeInSeconds;
-                comstockFactor.setText(String.format("%.3f", Factor));
+                comstockCalculation.setText(getString(R.string.text_factorCalculation, updatePoints, timeInSeconds));
+                timer.setText(getResources().getString(R.string.stopwatch_counter_zero,
+                        Minutes,
+                        Seconds,
+                        Miliseconds));
             }
         });
 
-// Tomek mnie męczy o gicie, a ja tu chce kodzic!
+        addAlphaButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePoints += getResources().getInteger(alpha_value);
+                comstockCalculation.setText(getString(R.string.text_factorCalculation, updatePoints, timeInSeconds));
+                if(timeInSeconds >0)
+                    Factor = (float) updatePoints / timeInSeconds;
+                else Factor = 0;
+                comstockFactor.setText(String.format(Locale.getDefault(),"%.3f", Factor));
+            }
+        });
+
+        addCharlieButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePoints += getResources().getInteger(charlie_value);
+                comstockCalculation.setText(getString(R.string.text_factorCalculation, updatePoints, timeInSeconds));
+                if(timeInSeconds >0)
+                    Factor = (float) updatePoints / timeInSeconds;
+                else Factor = 0;
+                comstockFactor.setText(String.format(Locale.getDefault(),"%.3f", Factor));
+            }
+        });
+
+        addDeltaButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePoints += getResources().getInteger(delta_value);
+                comstockCalculation.setText(getString(R.string.text_factorCalculation, updatePoints, timeInSeconds));
+                if(timeInSeconds >0)
+                    Factor = (float) updatePoints / timeInSeconds;
+                else Factor = 0;
+                comstockFactor.setText(String.format(Locale.getDefault(),"%.3f", Factor));
+            }
+        });
     }
+
     public Runnable runnableStopwatch = new Runnable(){
         public void run(){
             milisecondsTime = SystemClock.elapsedRealtime() - startTime;
@@ -100,7 +146,10 @@ public class MainActivity extends AppCompatActivity {
             Seconds = Seconds % 60;
             Miliseconds = (int)(updateTime % 1000);
             timeInSeconds = (float) (updateTime / 1000.0);
-            timer.setText(String.format("%02d", Minutes) + ":" + String.format("%02d", Seconds) + "." + String.format("%03d", Miliseconds));
+            timer.setText(getResources().getString(R.string.stopwatch_counter_zero,
+                    Minutes,
+                    Seconds,
+                    Miliseconds));
             stopwatchHandler.postDelayed(this, 0);
         }
     };
